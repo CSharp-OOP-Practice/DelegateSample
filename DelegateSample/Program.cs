d1b97f94437783550c7a8fcc2087d01757c38dda
RayW <ruizhouwu@gmail.com>
2017-09-08 14:27:49 +0800

    新增同委派定義簽章的方法給委派變數

--- DelegateSample/Program.cs
+++ DelegateSample/Program.cs
@@ -12,9 +12,15 @@ namespace DelegateSample
             // 符合委派定義的方法 指向給委派變數
             PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
             filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEye;
 
             // 傳遞的委派變數
             photoProcessor.Process("test", filterHandler);
+        }
+
+        static void RemoveRedEye(Photo photo)
+        {
+            Console.WriteLine("Remove red eye");
         }
     }
 }
