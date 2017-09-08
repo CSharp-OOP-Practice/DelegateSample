f28bfaf15e3fdc3b3fe2c1c91aa170e905e2e042
RayW <ruizhouwu@gmail.com>
2017-09-08 11:54:45 +0800

    init commit

---
+++ DelegateSample/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DelegateSample
+{
+    delegate string TestDelegate();
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestDelegate testDelegate = TestMethod;
+            Console.WriteLine(testDelegate());
+        }
+
+        private static string TestMethod()
+        {
+            return "測試委派成功";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+[TOC]
+
+---
+
+# Delegate
+
+## 說明
+
+1. An object that knows how to call a method (or a group of methods).
+2. A reference to a function.
+
+from Mosh Hamedani
+
+
+
